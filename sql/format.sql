8b9a11b4869f34cae3721a8fab2147ff9c52277e
summit45 <81628744+summit45@users.noreply.github.com>
2023-06-01 05:11:23 +0900

    Create format.sql

---
+++ sql/format.sql
@@ -0,0 +1,32 @@
+col c_id format a10;
+col c_id_no format 9999;
+col c_name format a20;
+col c_unit format 9999;
+
+col t_id format a10;
+col t_year format 9999;
+col t_semester format 9999;
+col t_time format a20;
+col t_max format 9999;
+col t_location format a15;
+col c_id format a10;
+col c_id_no format 9999;
+col p_id format a15;
+
+col p_id format a15;
+col p_name format a20;
+col p_pwd format a15;
+col p_email format a20;
+
+col e_id format a15;
+col e_year format 9999;
+col e_semester format 9999;
+col s_id format a15;
+col c_id format a10;
+col c_id_no format 9999;
+
+col s_id format a15;
+col s_pwd format a15;
+col s_name format a15;
+col s_email format a15;
+col s_address format a15;
